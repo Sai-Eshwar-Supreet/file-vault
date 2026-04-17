32ede72f38d1d074bfef26b5efb7e29eab01165e
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-04-17 16:41:58 +0000

    Add authentication middleware to home route

--- src/controllers/indexController.cjs
+++ src/controllers/indexController.cjs
@@ -1,5 +1,7 @@
+const { isAuthenticated } = require("../middlewares/auth.cjs");
+
 function getHome(req, res){
     res.render('pages/home');
 }
 
-module.exports.getHome = getHome;
+module.exports.getHome = [isAuthenticated, getHome];
